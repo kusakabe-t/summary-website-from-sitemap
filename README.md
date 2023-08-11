3e3e77c486d2745a0a726d914bc68764e2d32456
kusakabe <kusakabe.takuya@alleeks.com>
2023-08-12 00:56:12 +0900

    feat JSONファイルを生成できるようにしました

---
+++ README.md
@@ -0,0 +1,10 @@
+# ファイルの取得
+```shell
+$ deno run --allow-net --allow-read --allow-write main.ts --url <sitemap url>
+```
+
+# jsonファイルを要約
+
+```shell
+$ deno run --allow-net --allow-env --allow-read --allow-write main.ts --url <sitemap url>
+```
--- main.ts
+++ main.ts
@@ -1,5 +1,8 @@
 import { parse as cliParams } from "https://deno.land/std/flags/mod.ts";
 import { parse } from "https://deno.land/x/xml/mod.ts"
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+import { cheerio } from "https://deno.land/x/cheerio@1.0.7/mod.ts";
+import puppeteer from "https://deno.land/x/puppeteer@9.0.0/mod.ts";
 
 type XmlType = {
   xml: {
@@ -18,7 +21,7 @@ type XmlType = {
   }
 }
 
-const openAIApiKey = Deno.env.get('OPENAI_API_KEY')
+const openAIApiKey = config().OPENAI_API_KEY
 
 // Learn more at https://deno.land/manual/examples/module_metadata#concepts
 if (import.meta.main) {
@@ -35,7 +38,37 @@ if (import.meta.main) {
     const urlObjects = data.urlset.url
     const urls = urlObjects.map((urlObject) => urlObject.loc)
     console.warn('urls => ', urls)
-    // console.warn('data => ', data)
+    console.warn('openAIApiKey => ', openAIApiKey)
+
+    // const url1 = urls[5]
+    // const response = await fetch(url1)
+    // const html = await response.text()
+    // const $ = cheerio.load(html)
+
+    // console.warn('document => ', $('body').text())
+
+    let contents = []
+
+    for (const url of urls) {
+      const response = await fetch(url)
+      const html = await response.text()
+      const $ = cheerio.load(html)
+      const content = $('body').text()
+    
+      contents.push({ [url]: content })
+      // const encoder = new TextEncoder()
+      // const json = JSON.stringify({ document })
+      // console.warn('document => ', $('body').text())
+    }
+
+    console.warn('contents => ', contents)
+
+    // const encoder = new TextEncoder()
+    // const file = await Deno.open('contents.json', { write: true, create: true })
+    // await Deno.write(file, contents)
+    await Deno.writeTextFile('contents.json', JSON.stringify(contents))
+    console.log('file created.')
+    // file.close()
   } catch (error) {
     console.error(error)
   }
