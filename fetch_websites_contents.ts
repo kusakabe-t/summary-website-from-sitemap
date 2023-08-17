1decb9a84a37c1621aa3ea6995ec34882fdd18b1
kusakabe <kusakabe.takuya@alleeks.com>
2023-08-17 18:36:57 +0900

    refactoring

--- fetch_websites_contents.ts
+++ fetch_websites_contents.ts
@@ -1,41 +1,41 @@
 import { cliParams, cheerio, parse } from './depth.ts'
 import { XmlType } from './types.ts'
 
-if (import.meta.main) {
-  const args = cliParams(Deno.args);
-  const url = args.url
+const args = cliParams(Deno.args);
+const url = args.url
 
-  try {
-    const xml = await fetch(url).then((response) => response.text());
-    // parseメソッドの返り値の型を修正
-    // deno-lint-ignore ban-ts-comment
-    // @ts-ignore
-    const data = parse(xml) as XmlType
+try {
+  const xml = await fetch(url).then((response) => response.text());
+  // parseメソッドの返り値の型を修正
+  // deno-lint-ignore ban-ts-comment
+  // @ts-ignore
+  const data = parse(xml) as XmlType
 
-    // xml構造からurlのみを取得
-    const urlObjects = data.urlset.url
-    const urls = urlObjects.map((urlObject) => urlObject.loc)
+  // xml構造からurlのみを取得
+  const urlObjects = data.urlset.url
+  const urls = urlObjects.map((urlObject) => urlObject.loc)
 
-    const contents = []
+  const contents: { id: number, title: string, url: string, content: string }[] = []
 
-    for (const [index, url] of urls.entries()) {
-      console.log(url)
-      const response = await fetch(url)
-      const html = await response.text()
-      const $ = cheerio.load(html)
-      const rawContent = $('body').text()
+  for (const [index, url] of urls.entries()) {
+    console.log(url)
+    const response = await fetch(url)
+    const html = await response.text()
+    const $ = cheerio.load(html)
+    const rawContent = $('body').text()
 
-      // body内にあるjsonを消すために追加
-      const jsonContent = $('body')[0].children[1].children[0].data
-      const content = rawContent.replace(jsonContent, '')
+    const title = $('title').text()
 
-      console.log(`finish ${index + 1}/${urls.length}`)
-      contents.push({ id: index + 1, url, content })
-    }
+    // body内にあるjsonを消すために追加
+    const jsonContent = $('body')[0].children[1].children[0].data
+    const content = rawContent.replace(jsonContent, '')
 
-    await Deno.writeTextFile('contents.json', JSON.stringify(contents))
-    console.log('file created.')
-  } catch (error) {
-    console.error(error)
+    console.log(`finish ${index + 1}/${urls.length}`)
+    contents.push({ id: index + 1, title, url, content })
   }
+
+  await Deno.writeTextFile('contents.json', JSON.stringify(contents))
+  console.log('file created.')
+} catch (error) {
+  console.error(error)
 }
--- summary.ts
+++ summary.ts
@@ -7,34 +7,32 @@ const openAIApiKey = ''
 if (!contents) throw new Error('Please run fetch_websites_contents.')
 if (!openAIApiKey) throw new Error('Please Check OpenAI API Key.')
 
-if (import.meta.main) {
-	// OpenAIへの指示
-  const llm = new ChatOpenAI({
-    modelName: 'gpt-4-0613',
-    openAIApiKey,
-    prefixMessages: [{ role: 'system', content: '日本語で回答してください。'}]
-  })
-	const prompt = new PromptTemplate({ template: "{text}を300文字以内で要約してください。", inputVariables: ['text'] })
-	const chain = loadSummarizationChain(llm, { type: "map_reduce", combineMapPrompt: prompt, combinePrompt: prompt })
+// OpenAIへの指示
+const llm = new ChatOpenAI({
+	modelName: 'gpt-4-0613',
+	openAIApiKey,
+	prefixMessages: [{ role: 'system', content: '日本語で回答してください。'}]
+})
+const prompt = new PromptTemplate({ template: "{text}を300文字以内で要約してください。", inputVariables: ['text'] })
+const chain = loadSummarizationChain(llm, { type: "map_reduce", combineMapPrompt: prompt, combinePrompt: prompt })
 
-	// 要約する対象の文を分割
-	const textSplitter = new CharacterTextSplitter({ chunkSize: 8000, chunkOverlap: 500 })
+// 要約する対象の文を分割
+const textSplitter = new CharacterTextSplitter({ chunkSize: 8000, chunkOverlap: 500 })
 
-	const summaryContents = []
+const summaryContents: { id: number, title: string, url: string, content: string, summary: string }[] = []
 
-	for (const [index, rawContent] of contents.entries()) {
-		console.log('start: ', rawContent.url)
-		// contents.jsonの内容を分割し、ChatGPTで要約
-		const texts = await textSplitter.splitText(rawContent.content)
-		const document = texts.map((content: string) => new Document({ pageContent: content }))
-		const summaryResponse = await chain.call({ input_documents: document })
+for (const [index, rawContent] of contents.entries()) {
+	console.log('start: ', rawContent.url)
+	// contents.jsonの内容を分割し、ChatGPTで要約
+	const texts = await textSplitter.splitText(rawContent.content)
+	const document = texts.map((content: string) => new Document({ pageContent: content }))
+	const summaryResponse = await chain.call({ input_documents: document })
 
-		// 要約結果の表示
-		console.log(`finish: ${index + 1}/${contents.length}`)
-		console.warn('summary', summaryResponse.text)
-		summaryContents.push({ id: rawContent.id, url: rawContent.url, content: rawContent.content, summary: summaryResponse.text })	
-	}
-
-	await Deno.writeTextFile('summary.json', JSON.stringify(summaryContents))
-	console.log('file created.')
+	// 要約結果の表示
+	console.log(`finish: ${index + 1}/${contents.length}`)
+	console.warn('summary', summaryResponse.text)
+	summaryContents.push({ id: rawContent.id, title: rawContent.title, url: rawContent.url, content: rawContent.content, summary: summaryResponse.text })	
 }
+
+await Deno.writeTextFile('summary.json', JSON.stringify(summaryContents))
+console.log('file created.')
